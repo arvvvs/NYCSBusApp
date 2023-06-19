44abca5a336006956c7a59da8532d32b8afa01eb
Ubuntu <ubuntu@ip-172-31-7-168.ec2.internal>
2023-06-19 19:41:59 +0000

    updated requirements and added new file to constants

--- data/CONSTANTS.py
+++ data/CONSTANTS.py
@@ -4,6 +4,7 @@ BATTERY_RAW_DATA_FOLDER = "1rewZIceeVQMDDhnBw2_xLBZGJ-f5oJFU"
 RPM_RAW_DATA_FOLDER = "14SsAEseb_3GBEHp0Ucpq6tupTDYsDKeF"
 # https://drive.google.com/file/d/1YzjoEREz2-sBVhqd2-4nnfFFjFDdWK3g/view?usp=drive_link
 GEOTAB_MAPPINGS_CSV = "1YzjoEREz2-sBVhqd2-4nnfFFjFDdWK3g"
+BUS_METADATA_MAPPINGS_CSV = "1GaSKRwmYpim2IAKQYO_nJZyHVmJQ65Vj"
 # https://drive.google.com/drive/u/0/folders/1m85Uhkqe6glbcMOj2uaAqxAyX2ZWu-8z
 METRICS_FINALIZED_DATA_FOLDER = "1m85Uhkqe6glbcMOj2uaAqxAyX2ZWu-8z"
 BATTERY_VIEW_DATA_CSV = "1KtieUwZbS5ft2BHJQn0py47bbwN7TAQL"
