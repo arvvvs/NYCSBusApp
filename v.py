35be570160e5e17aa2599cbd185214bbe4f8917f
Ananya Rajesh <61556028+ananyarajesh@users.noreply.github.com>
2023-06-19 15:47:48 -0400

    Add files via upload

--- v.py
+++ v.py
@@ -1,83 +1,49 @@
-import streamlit as st
-import pandas as pd
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+import numpy as np
+from sklearn.ensemble import IsolationForest
 
-# Load data from the shared file
-def load_data(file_id):
-    file_data = drive_service.files().get_media(fileId=file_id).execute()
-    df = pd.read_csv(file_data.decode('utf-8'))
-    return df
-
-# Convert estDateTime column to datetime
-def convert_to_datetime(df):
-    df['estDateTime'] = pd.to_datetime(df['estDateTime'], format='%Y-%m-%d %H:%M:%S.%f%z')
-    return df
-
-# Detect anomalies using Isolation Forest
-def detect_anomalies(df):
-    # Select the column for anomaly detection (estDateTime)
-    data = df[['estDateTime']]
-    
-    # Fit the Isolation Forest model
-    model = IsolationForest(contamination=0.1, random_state=42)
-    model.fit(data)
-    
-    # Predict anomaly scores
-    scores = model.decision_function(data)
-    df['anomaly_score'] = scores
-    
-    # Set a threshold for anomaly detection
-    threshold = df['anomaly_score'].quantile(0.9)
-    
-    # Flag anomalies
-    df['anomaly'] = df['anomaly_score'] > threshold
-    
-    return df
-
-# Run the Streamlit app
-def run_streamlit_app():
-    st.title("Anomaly Detection")
-    
-    # Example: Search for a file in a shared folder by name
-    folder_id = '1m85Uhkqe6glbcMOj2uaAqxAyX2ZWu-8z'  # Replace with the ID of the shared folder
-    file_name = 'battery_view_data.csv'  # Replace with the name of the file you want to access
-    
-    # Search for files in the shared folder
-    results = drive_service.files().list(q=f"'{folder_id}' in parents").execute()
-    files = results.get('files', [])
-    
-    # Find the specific file within the shared folder
-    file_id = None
-    for file in files:
-        if file['name'] == file_name:
-            file_id = file['id']
-            break
-    
-    # Read file contents into a Pandas DataFrame if found
-    if file_id:
-        df = load_data(file_id)
-        
-        # Convert estDateTime column to datetime
-        df = convert_to_datetime(df)
-        
-        # Display a preview of the data
-        st.subheader("Data Preview")
-        st.write(df.head())  # Display the first few rows
-        
-        # Detect anomalies and perform further analysis
-        df = detect_anomalies(df)
-        
-        # Display the DataFrame with anomalies flagged
-        st.subheader("Data with Anomalies")
-        st.dataframe(df)
-        
-    else:
-        st.error('File not found in the shared folder.')
+from data.CONSTANTS import BATTERY_VIEW_DATA_CSV
+from data.utilities import get_csv_from_drive_as_dataframe
 
 
-# Authenticate and run the Streamlit app
-flow = InstalledAppFlow.from_client_secrets_file('/Users/ananyarajesh/Desktop/NYCSBUS/client_secret_33984701204-qjcuo9cfb8q2p5la1n140skh2b6k8425.apps.googleusercontent.com.json', ['https://www.googleapis.com/auth/drive'])
-credentials = flow.run_local_server(port=0)
-drive_service = build('drive', 'v3', credentials=credentials)
-run_streamlit_app()
+def main():
+    # Retrieve the file ID from the CONSTANTS module
+    file_id = BATTERY_VIEW_DATA_CSV
+
+    # Retrieve the file data from Google Drive
+    file_data = get_csv_from_drive_as_dataframe(file_id)
+    print(file_data)
+
+    if file_data is not None:
+        # Process the file data and detect outliers
+        outliers = detect_outliers(file_data)
+
+        # Print the outliers
+        print("Detected outliers:")
+        for outlier in outliers:
+            print(outlier)
+
+
+def detect_outliers(data):
+    # Convert data to numpy array
+    X = data.values.reshape(-1, 1)
+
+    # Initialize and fit the Isolation Forest model
+    model = IsolationForest(
+        contamination=0.05
+    )  # Adjust the contamination parameter as needed
+    model.fit(X)
+
+    # Predict outlier labels (-1 for outliers, 1 for inliers)
+    labels = model.predict(X)
+
+    # Get indices of outliers
+    outlier_indices = np.where(labels == -1)[0]
+
+    # Get the outlier values
+    outliers = [data.iloc[i, 0] for i in outlier_indices]
+
+    return outliers
+
+
+if __name__ == "__main__":
+    main()
